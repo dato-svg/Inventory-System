dead1e54d9d6dbf2d335d71923e942ae8bc23779
dato-svg <139239441+dato-svg@users.noreply.github.com>
2025-03-01 18:50:26 +0400

    last update
    last update

--- Assets/Scripts/Inventory/InventorySystem.cs
+++ Assets/Scripts/Inventory/InventorySystem.cs
@@ -7,20 +7,56 @@ using UnityEngine;
 public class InventorySystem : ScriptableObject
 {
     public InventorySlot[] slots = new InventorySlot[30];
+    
+
     public int unlockedSlots = 15;
     public int unlockPrice = 100;
     public int playerCoins = 500;
 
-  /*  private void OnEnable()
-    {
-        
-        for (int i = 0; i < slots.Length; i++)
-        {
-            slots[i] = new InventorySlot();
-        }
-    }*/
 
-    
+    public float GetTotalWeight()
+    {
+        float totalWeight = 0f;
+        foreach (var slot in slots)
+        {
+            if (slot.item != null)
+            {
+                totalWeight += slot.item.weight * slot.count;
+            }
+        }
+        
+        return totalWeight;
+    }
+
+
+    public int GetTotalDefense()
+    {
+        int totalDefense = 0;
+        foreach (var slot in slots)
+        {
+            if (slot.item is ArmorItem armor)
+            {
+                totalDefense += armor.defense * slot.count;
+            }
+        }
+        return totalDefense;
+    }
+
+    public int GetTotalAttack()
+    {
+        int totalAttack = 0;
+        foreach (var slot in slots)
+        {
+            if (slot.item is WeaponItem weapon)
+            {
+                totalAttack += weapon.attackPower * slot.count;
+            }
+        }
+        return totalAttack;
+    }
+
+
+
 
     public void UnlockFirstSlots()
     {
@@ -38,6 +74,7 @@ public class InventorySystem : ScriptableObject
         {
             if (slot.IsUnlocked && slot.AddItem(item, amount))
             {
+                Debug.Log($"Общий вес: {GetTotalWeight()} | Общая броня: {GetTotalDefense()} | Общий урон: {GetTotalAttack()}");
                 return true;
             }
         }
@@ -49,6 +86,7 @@ public class InventorySystem : ScriptableObject
         if (slotIndex >= 0 && slotIndex < slots.Length && slots[slotIndex].IsUnlocked)
         {
             slots[slotIndex].Clear();
+            Debug.Log($"Общий вес: {GetTotalWeight()} | Общая броня: {GetTotalDefense()} | Общий урон: {GetTotalAttack()}");
         }
     }
 
